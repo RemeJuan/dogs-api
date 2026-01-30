ef6107e9265274c3a4696cc4986899f9d0c01a75
Reme Le Hane <mail@remej.dev>
2026-01-30 08:46:18 +0200

    feat(api) favorites shared interface

---
+++ libs/shared-interfaces/src/lib/favourites.ts
@@ -0,0 +1,19 @@
+export interface Favourite {
+  id: string;
+  breed: string;
+  imageUrl: string;
+  createdAt: Date;
+}
+
+export interface FavouritesResponse {
+  favourites: Favourite[];
+}
+
+export interface AddFavouriteRequest {
+  breed: string;
+  imageUrl: string;
+}
+
+export interface AddFavouriteResponse {
+  favourite: Favourite;
+}
