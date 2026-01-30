3db638a35826bc77d270df987dd900874c4f356b
Reme Le Hane <mail@remej.dev>
2026-01-30 19:59:32 +0200

    feat(project) setting up context to track selected breed between components

--- apps/web/src/pages/home/home.page.tsx
+++ apps/web/src/pages/home/home.page.tsx
@@ -1,22 +1,22 @@
-import { Typography, Sheet, Grid } from '@mui/joy';
+import { Sheet, Grid } from '@mui/joy';
 import { BreedsList } from './components/breeds-list.component';
+import { HomeContextProvider } from '@web/pages/home/home.context';
+import { BreedImages } from '@web/pages/home/components/breed-images.component';
 
 export function HomePage() {
   return (
-    <Grid container spacing={3} sx={{ mb: 4 }}>
-      <Grid xs={12} md={3}>
-        <Sheet variant="plain" sx={{ p: 3, borderRadius: 'md' }}>
-          <BreedsList />
-        </Sheet>
-      </Grid>
+    <HomeContextProvider>
+      <Grid container spacing={3} sx={{ mb: 4 }}>
+        <Grid xs={12} md={3}>
+          <Sheet variant="plain" sx={{ p: 3, borderRadius: 'md' }}>
+            <BreedsList />
+          </Sheet>
+        </Grid>
 
-      <Grid xs={12} md={9}>
-        <Sheet variant="outlined" sx={{ p: 4, borderRadius: 'md' }}>
-          <Typography level="h2" component="h1" sx={{ mb: 2 }}>
-            Welcome to Dog Breeds Explorer
-          </Typography>
-        </Sheet>
+        <Grid xs={12} md={9}>
+          <BreedImages />
+        </Grid>
       </Grid>
-    </Grid>
+    </HomeContextProvider>
   );
 }
