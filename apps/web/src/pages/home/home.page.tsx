23fd69674c2b9ec386027d6288e121edba9bcd25
Reme Le Hane <remelehane@capitecbank.co.za>
2026-01-31 07:44:52 +0200

    feat(project) implementing image display upon breed selection

--- apps/web/src/pages/home/home.page.tsx
+++ apps/web/src/pages/home/home.page.tsx
@@ -1,4 +1,4 @@
-import { Sheet, Grid } from '@mui/joy';
+import { Sheet, Grid, Typography } from '@mui/joy';
 import { BreedsList } from './components/breeds-list.component';
 import { HomeContextProvider } from '@web/pages/home/home.context';
 import { BreedImages } from '@web/pages/home/components/breed-images.component';
@@ -8,13 +8,24 @@ export function HomePage() {
     <HomeContextProvider>
       <Grid container spacing={3} sx={{ mb: 4 }}>
         <Grid xs={12} md={3}>
-          <Sheet variant="plain" sx={{ p: 3, borderRadius: 'md' }}>
+          <Sheet
+            variant="plain"
+            sx={{ p: 3, borderRadius: 'md', height: '100%' }}
+          >
             <BreedsList />
           </Sheet>
         </Grid>
 
         <Grid xs={12} md={9}>
-          <BreedImages />
+          <Sheet
+            variant="outlined"
+            sx={{ p: 4, borderRadius: 'md', height: '100%' }}
+          >
+            <Typography level="h2" component="h1" sx={{ mb: 2 }}>
+              Welcome to Dog Breeds Explorer
+            </Typography>
+            <BreedImages />
+          </Sheet>
         </Grid>
       </Grid>
     </HomeContextProvider>
